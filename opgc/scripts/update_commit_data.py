e4bc2d880d0cf5a46a0e99711976ca0942cb43c2
JayJi <ckj9014@gmail.com>
2023-01-09 02:53:04 +0900

    feat: 커밋 데이터 업데이트 스크립트 추가

---
+++ opgc/scripts/update_commit_data.py
@@ -0,0 +1,19 @@
+from apps.githubs.models import GithubUser
+from core.services.github_service import GithubInformationService
+from utils.github import get_continuous_commit_day
+
+
+def run():
+    username = input("input username: ")
+
+    try:
+        github_user = GithubUser.objects.get(username=username)
+        is_completed, continuous_count = get_continuous_commit_day(username)
+
+        if is_completed:
+            github_user.continuous_commit_day = continuous_count
+            github_user.total_score = GithubInformationService.get_total_score(github_user)
+            github_user.save(update_fields=['continuous_commit_day', 'total_score'])
+
+    except GithubUser.DoesNotExist:
+        pass

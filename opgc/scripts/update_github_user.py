03b99c4d629ab0419a8185eb5461b287653c0d7d
JayJi <ckj9014@gmail.com>
2023-01-09 02:49:39 +0900

    faat: update user 스크립트 수정

--- opgc/scripts/update_github_user.py
+++ opgc/scripts/update_github_user.py
@@ -1,14 +1,22 @@
 from apps.githubs.models import GithubUser
 from core.services.github_service import GithubInformationService
+from utils.github import get_continuous_commit_day
 
 
 def run():
-    username = input("input delete username: ")
+    username = input("input username: ")
 
     try:
-        GithubUser.objects.get(username=username)
+        github_user = GithubUser.objects.get(username=username)
         github_information_service = GithubInformationService(username=username)
         github_information_service.update()
+
+        is_completed, continuous_count = get_continuous_commit_day(username)
+
+        if is_completed:
+            github_user.continuous_commit_day = continuous_count
+            github_user.total_score = GithubInformationService.get_total_score(github_user)
+            github_user.save(update_fields=['continuous_commit_day', 'total_score'])
 
     except GithubUser.DoesNotExist:
         pass
